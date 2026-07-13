6820769e9e09239c2931c04d50497cd92f11c054
Dravia Vemal, Murugesan <contact@draviavemal.com>
2026-07-13 10:27:50 +0000

    organising FFI to best maintain the future structure support

--- cs/openxml_office/spreadsheet/Excel.cs
+++ cs/openxml_office/spreadsheet/Excel.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 using Google.FlatBuffers;
 using draviavemal.openxml_office.global_2007;
-using openxml_office_fbs.spreadsheet_2007;
+using openxml_office_fbs.spreadsheet;
 
 namespace draviavemal.openxml_office.spreadsheet_2007
 {
@@ -12,61 +12,87 @@ namespace draviavemal.openxml_office.spreadsheet_2007
     /// </summary>
     public class Excel : PrivacyProperties
     {
-        private readonly IntPtr ffiExcel;
+        private readonly ulong ffiExcelPtr;
+
+        private delegate sbyte FfiBufferCall(
+            IntPtr inBuffer,
+            UIntPtr inBufferSize,
+            out IntPtr outBuffer,
+            out UIntPtr outBufferSize,
+            out IntPtr errorMsg
+        );
+
+        private delegate sbyte FfiVoidCall(
+            IntPtr inBuffer,
+            UIntPtr inBufferSize,
+            out IntPtr errorMsg
+        );
 
         /// <summary>
         /// 
         /// </summary>
-        [DllImport("lib/draviavemal_openxml_office_ffi", CallingConvention = CallingConvention.Cdecl)]
-        public static extern sbyte excel_create(
-            [MarshalAs(UnmanagedType.LPStr)] string optional_file_path,
+        [DllImport("lib/draviavemal_openxml_office_ffi", EntryPoint = "excel_create", CallingConvention = CallingConvention.Cdecl)]
+        private static extern sbyte ffi_excel_create(
+            IntPtr in_buffer,
+            UIntPtr in_buffer_size,
+            out IntPtr out_buffer,
+            out UIntPtr out_buffer_size,
+            out IntPtr error_msg
+        );
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DllImport("lib/draviavemal_openxml_office_ffi", EntryPoint = "excel_add_sheet", CallingConvention = CallingConvention.Cdecl)]
+        private static extern sbyte ffi_excel_add_sheet(
+            IntPtr in_buffer,
+            UIntPtr in_buffer_size,
+            out IntPtr out_buffer,
+            out UIntPtr out_buffer_size,
+            out IntPtr error_msg
+        );
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DllImport("lib/draviavemal_openxml_office_ffi", EntryPoint = "excel_get_sheet", CallingConvention = CallingConvention.Cdecl)]
+        private static extern sbyte ffi_excel_get_sheet(
+            IntPtr in_buffer,
+            UIntPtr in_buffer_size,
+            out IntPtr out_buffer,
+            out UIntPtr out_buffer_size,
+            out IntPtr error_msg
+        );
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DllImport("lib/draviavemal_openxml_office_ffi", EntryPoint = "excel_rename_sheet", CallingConvention = CallingConvention.Cdecl)]
+        private static extern sbyte ffi_excel_rename_sheet(
+            IntPtr in_buffer,
+            UIntPtr in_buffer_size,
+            out IntPtr error_msg
+        );
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DllImport("lib/draviavemal_openxml_office_ffi", EntryPoint = "excel_save_as", CallingConvention = CallingConvention.Cdecl)]
+        private static extern sbyte ffi_excel_save_as(
+            IntPtr in_buffer,
+            UIntPtr in_buffer_size,
+            out IntPtr out_buffer,
+            out UIntPtr out_buffer_size,
+            out IntPtr error_msg
+        );
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DllImport("lib/draviavemal_openxml_office_ffi", EntryPoint = "free_buffer", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void ffi_free_buffer(
             IntPtr buffer,
-            int length,
-            out IntPtr excel,
-            out IntPtr error_msg
-        );
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [DllImport("lib/draviavemal_openxml_office_ffi", CallingConvention = CallingConvention.Cdecl)]
-        public static extern sbyte excel_add_sheet(
-            IntPtr excelPtr,
-            [MarshalAs(UnmanagedType.LPStr)] string optional_sheet_name,
-            out IntPtr worksheet,
-            out IntPtr error_msg
-        );
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [DllImport("lib/draviavemal_openxml_office_ffi", CallingConvention = CallingConvention.Cdecl)]
-        public static extern sbyte excel_get_sheet(
-            IntPtr excelPtr,
-            [MarshalAs(UnmanagedType.LPStr)] string sheet_name,
-            out IntPtr worksheet,
-            out IntPtr error_msg
-        );
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [DllImport("lib/draviavemal_openxml_office_ffi", CallingConvention = CallingConvention.Cdecl)]
-        public static extern sbyte excel_rename_sheet(
-            IntPtr excelPtr,
-            [MarshalAs(UnmanagedType.LPStr)] string old_sheet_name,
-            [MarshalAs(UnmanagedType.LPStr)] string new_sheet_name,
-            out IntPtr error_msg
-        );
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [DllImport("lib/draviavemal_openxml_office_ffi", CallingConvention = CallingConvention.Cdecl)]
-        public static extern sbyte excel_save_as(
-            IntPtr excelPtr,
-            [MarshalAs(UnmanagedType.LPStr)] string file_name,
-            out IntPtr error_msg
+            UIntPtr buffer_size
         );
 
         /// <summary>
@@ -75,26 +101,7 @@ namespace draviavemal.openxml_office.spreadsheet_2007
         /// </summary>
         public Excel(ExcelProperties excelProperties = null)
         {
-            if (excelProperties == null)
-            {
-                excelProperties = new ExcelProperties();
-            }
-            FlatBufferBuilder builder = new(1024);
-            ExcelPropertiesModel.StartExcelPropertiesModel(builder);
-            ExcelPropertiesModel.AddIsEditable(builder, true);
-            Offset<ExcelPropertiesModel> excelPropertiesModel = ExcelPropertiesModel.EndExcelPropertiesModel(builder);
-            builder.Finish(excelPropertiesModel.Value);
-            byte[] buffer = builder.SizedByteArray();
-            int bufferSize = buffer.Length;
-            unsafe
-            {
-                fixed (byte* bufferPtr = buffer)
-                {
-                    IntPtr bufferIntPtr = (IntPtr)bufferPtr;
-                    sbyte statusCode = excel_create(null, bufferIntPtr, bufferSize, out ffiExcel, out IntPtr errorMsg);
-                    StatusCode.ProcessStatusCode(statusCode, errorMsg);
-                }
-            }
+            ffiExcelPtr = CreateExcel(null, excelProperties);
         }
 
         /// <summary>
@@ -104,26 +111,23 @@ namespace draviavemal.openxml_office.spreadsheet_2007
 		/// </summary>
         public Excel(string fileName, ExcelProperties excelProperties = null)
         {
+            ffiExcelPtr = CreateExcel(fileName, excelProperties);
+        }
+
+        private static ulong CreateExcel(string fileName, ExcelProperties excelProperties)
+        {
             if (excelProperties == null)
             {
                 excelProperties = new ExcelProperties();
             }
             FlatBufferBuilder builder = new(1024);
-            ExcelPropertiesModel.StartExcelPropertiesModel(builder);
-            ExcelPropertiesModel.AddIsEditable(builder, true);
-            Offset<ExcelPropertiesModel> excelPropertiesModel = ExcelPropertiesModel.EndExcelPropertiesModel(builder);
-            builder.Finish(excelPropertiesModel.Value);
-            byte[] buffer = builder.SizedByteArray();
-            int bufferSize = buffer.Length;
-            unsafe
-            {
-                fixed (byte* bufferPtr = buffer)
-                {
-                    IntPtr bufferIntPtr = (IntPtr)bufferPtr;
-                    sbyte statusCode = excel_create(fileName, bufferIntPtr, bufferSize, out ffiExcel, out IntPtr errorMsg);
-                    StatusCode.ProcessStatusCode(statusCode, errorMsg);
-                }
-            }
+            StringOffset fileNameOffset = fileName != null ? builder.CreateString(fileName) : default;
+            Offset<excel_settings> excelSettingsOffset = excel_settings.Createexcel_settings(builder, true);
+            Offset<excel_create> excelCreateOffset = excel_create.Createexcel_create(builder, fileNameOffset, excelSettingsOffset);
+            builder.Finish(excelCreateOffset.Value);
+            byte[] responseBuffer = InvokeBufferFfi(ffi_excel_create, builder);
+            excel_create_return response = excel_create_return.GetRootAsexcel_create_return(new ByteBuffer(responseBuffer));
+            return response.ExcelPtr;
         }
 
         /// <summary>
@@ -142,9 +146,13 @@ namespace draviavemal.openxml_office.spreadsheet_2007
         /// <returns></returns>
         public Worksheet AddSheet(string sheetName)
         {
-            sbyte statusCode = excel_add_sheet(ffiExcel, sheetName, out IntPtr ffiWorksheet, out IntPtr errorMsg);
-            StatusCode.ProcessStatusCode(statusCode, errorMsg);
-            return new Worksheet(ffiWorksheet);
+            FlatBufferBuilder builder = new(1024);
+            StringOffset sheetNameOffset = sheetName != null ? builder.CreateString(sheetName) : default;
+            Offset<excel_add_sheet> addSheetOffset = excel_add_sheet.Createexcel_add_sheet(builder, ffiExcelPtr, sheetNameOffset);
+            builder.Finish(addSheetOffset.Value);
+            byte[] responseBuffer = InvokeBufferFfi(ffi_excel_add_sheet, builder);
+            excel_add_sheet_return response = excel_add_sheet_return.GetRootAsexcel_add_sheet_return(new ByteBuffer(responseBuffer));
+            return new Worksheet(new IntPtr((long)response.WorksheetPtr));
         }
 
         /// <summary>
@@ -154,9 +162,13 @@ namespace draviavemal.openxml_office.spreadsheet_2007
         /// <returns></returns>
         public Worksheet GetWorksheet(string sheetName)
         {
-            sbyte statusCode = excel_get_sheet(ffiExcel, sheetName, out IntPtr ffiWorksheet, out IntPtr errorMsg);
-            StatusCode.ProcessStatusCode(statusCode, errorMsg);
-            return new Worksheet(ffiWorksheet);
+            FlatBufferBuilder builder = new(1024);
+            StringOffset sheetNameOffset = builder.CreateString(sheetName);
+            Offset<excel_get_sheet> getSheetOffset = excel_get_sheet.Createexcel_get_sheet(builder, ffiExcelPtr, sheetNameOffset);
+            builder.Finish(getSheetOffset.Value);
+            byte[] responseBuffer = InvokeBufferFfi(ffi_excel_get_sheet, builder);
+            excel_get_sheet_return response = excel_get_sheet_return.GetRootAsexcel_get_sheet_return(new ByteBuffer(responseBuffer));
+            return new Worksheet(new IntPtr((long)response.WorksheetPtr));
         }
 
         /// <summary>
@@ -166,8 +178,12 @@ namespace draviavemal.openxml_office.spreadsheet_2007
         /// <param name="newSheetName"></param>
         public bool RenameSheet(string oldSheetName, string newSheetName)
         {
-            sbyte statusCode = excel_rename_sheet(ffiExcel, oldSheetName, newSheetName, out IntPtr errorMsg);
-            StatusCode.ProcessStatusCode(statusCode, errorMsg);
+            FlatBufferBuilder builder = new(1024);
+            StringOffset oldSheetNameOffset = builder.CreateString(oldSheetName);
+            StringOffset newSheetNameOffset = builder.CreateString(newSheetName);
+            Offset<excel_rename_sheet> renameSheetOffset = excel_rename_sheet.Createexcel_rename_sheet(builder, ffiExcelPtr, oldSheetNameOffset, newSheetNameOffset);
+            builder.Finish(renameSheetOffset.Value);
+            InvokeVoidFfi(ffi_excel_rename_sheet, builder);
             return true;
         }
 
@@ -178,8 +194,56 @@ namespace draviavemal.openxml_office.spreadsheet_2007
         /// </summary>
         public void SaveAs(string filePath)
         {
-            sbyte statusCode = excel_save_as(ffiExcel, filePath, out IntPtr errorMsg);
-            StatusCode.ProcessStatusCode(statusCode, errorMsg);
+            FlatBufferBuilder builder = new(1024);
+            StringOffset filePathOffset = builder.CreateString(filePath);
+            Offset<excel_save_as> saveAsOffset = excel_save_as.Createexcel_save_as(builder, ffiExcelPtr, filePathOffset);
+            builder.Finish(saveAsOffset.Value);
+            InvokeBufferFfi(ffi_excel_save_as, builder);
+        }
+
+        private static byte[] InvokeBufferFfi(FfiBufferCall ffiCall, FlatBufferBuilder builder)
+        {
+            byte[] requestBuffer = builder.SizedByteArray();
+            unsafe
+            {
+                fixed (byte* requestPtr = requestBuffer)
+                {
+                    sbyte statusCode = ffiCall(
+                        (IntPtr)requestPtr,
+                        new UIntPtr((uint)requestBuffer.Length),
+                        out IntPtr responsePtr,
+                        out UIntPtr responseSize,
+                        out IntPtr errorMsg);
+                    StatusCode.ProcessStatusCode(statusCode, errorMsg);
+                    int responseLength = (int)responseSize.ToUInt64();
+                    byte[] responseBuffer = new byte[responseLength];
+                    if (responseLength > 0)
+                    {
+                        Marshal.Copy(responsePtr, responseBuffer, 0, responseLength);
+                    }
+                    if (responsePtr != IntPtr.Zero)
+                    {
+                        ffi_free_buffer(responsePtr, responseSize);
+                    }
+                    return responseBuffer;
+                }
+            }
+        }
+
+        private static void InvokeVoidFfi(FfiVoidCall ffiCall, FlatBufferBuilder builder)
+        {
+            byte[] requestBuffer = builder.SizedByteArray();
+            unsafe
+            {
+                fixed (byte* requestPtr = requestBuffer)
+                {
+                    sbyte statusCode = ffiCall(
+                        (IntPtr)requestPtr,
+                        new UIntPtr((uint)requestBuffer.Length),
+                        out IntPtr errorMsg);
+                    StatusCode.ProcessStatusCode(statusCode, errorMsg);
+                }
+            }
         }
 
     }
--- rs_ffi/src/utils.rs
+++ rs_ffi/src/utils.rs
@@ -1,6 +1,12 @@
-use std::ffi::{c_char, CString};
+use std::{
+    ffi::{c_char, CString},
+    slice::from_raw_parts,
+};
 
 use anyhow::Error as AnyError;
+use flatbuffers::{Follow, Verifiable};
+
+use crate::StatusCode;
 
 pub(crate) fn chain_error(error: &AnyError) -> *mut c_char {
     let mut message = String::new();
@@ -17,4 +23,75 @@ pub(crate) fn chain_error(error: &AnyError) -> *mut c_char {
             std::ptr::null_mut()
         }
     }
+}
+
+/// Validates the incoming FFI buffer and parses it as the given FlatBuffers root type.
+///
+/// On success returns the parsed root. On failure sets `out_error` (for parse errors)
+/// and returns the `StatusCode` (as `i8`) that the caller should return.
+///
+/// # Safety
+/// `in_buffer` must point to `in_buffer_size` valid bytes for the duration of the call,
+/// and `out_error` must be a valid pointer.
+pub(crate) unsafe fn root_from_raw<'a, T>(
+    in_buffer: *const u8,
+    in_buffer_size: usize,
+    out_error: *mut *const c_char,
+) -> Result<T::Inner, i8>
+where
+    T: Follow<'a> + Verifiable + 'a,
+{
+    if in_buffer.is_null() || in_buffer_size == 0 {
+        return Err(StatusCode::InvalidArgument as i8);
+    }
+    let buffer_slice = from_raw_parts(in_buffer, in_buffer_size);
+    match flatbuffers::root::<T>(buffer_slice) {
+        Ok(root) => Ok(root),
+        Err(e) => Err(set_error(out_error, &e.into(), StatusCode::FlatBufferError)),
+    }
+}
+
+/// Writes a serialized FlatBuffers payload to the FFI output pointers.
+///
+/// The payload is copied into a freshly allocated heap buffer whose ownership is
+/// transferred to the caller. The caller must release it by calling `free_buffer`
+/// with the returned pointer and size once the bytes have been consumed.
+///
+/// # Safety
+/// `out_buffer` and `out_buffer_size` must be valid pointers.
+pub(crate) unsafe fn write_buffer(
+    buf: &[u8],
+    out_buffer: *mut *mut u8,
+    out_buffer_size: *mut usize,
+) {
+    let boxed: Box<[u8]> = Box::from(buf);
+    *out_buffer_size = boxed.len();
+    *out_buffer = Box::into_raw(boxed) as *mut u8;
+}
+
+#[no_mangle]
+/// Releases a buffer previously handed to the caller through an FFI output pointer.
+///
+/// # Safety
+/// `buffer` must be a pointer returned by an FFI call together with its matching
+/// `buffer_size`, and it must not have been freed already.
+pub unsafe extern "C" fn free_buffer(buffer: *mut u8, buffer_size: usize) {
+    if buffer.is_null() {
+        return;
+    }
+    let slice = std::slice::from_raw_parts_mut(buffer, buffer_size);
+    drop(Box::from_raw(slice as *mut [u8]));
+}
+
+/// Sets the FFI error output from an error chain and returns the given status code.
+///
+/// # Safety
+/// `out_error` must be a valid pointer.
+pub(crate) unsafe fn set_error(
+    out_error: *mut *const c_char,
+    error: &AnyError,
+    status_code: StatusCode,
+) -> i8 {
+    *out_error = chain_error(error);
+    status_code as i8
 }
