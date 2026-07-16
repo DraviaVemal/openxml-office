132c01ff5011eabeb030de79f3ec59ce50862d1e
Dravia Vemal, Murugesan <contact@draviavemal.com>
2026-07-16 01:03:01 +0000

    reorganising directory structure to reduce complex navigation

--- build.sh
+++ build.sh
@@ -19,14 +19,14 @@ done
 
 # Define the source and target directories
 SOURCE_DIR="fbs"
-C_SHARP_DIR="cs/openxml_office"
+C_SHARP_DIR="cs"
 JAVA_DIR="java/draviavemal_openxml_office/src/main/java"
 PYTHON_DIR="python"
 GO_DIR="go"
 
 # Flatbuffer Generated code setup
 rm -rf "$C_SHARP_DIR/openxml_office_fbs"
-rm -rf "$GO_DIR/openxml_office_fbs"
+rm -rf "$GO_DIR/internal/openxml_office_fbs"
 rm -rf "$JAVA_DIR/openxml_office_fbs"
 rm -rf "$PYTHON_DIR/openxml_office_fbs"
 
@@ -35,7 +35,7 @@ rm -rf target
 
 # Create the corresponding output directory in TARGET_DIR
 mkdir -p "$C_SHARP_DIR"
-mkdir -p "$GO_DIR"
+mkdir -p "$GO_DIR/internal"
 mkdir -p "$JAVA_DIR/openxml_office_fbs"
 mkdir -p "$PYTHON_DIR/openxml_office_fbs"
 
@@ -49,7 +49,7 @@ find "$SOURCE_DIR" -name "*.fbs" | while read -r fbs_file; do
   flatc -j -o "$JAVA_DIR" "$fbs_file"
 done
 # Rust code get complied as one source to maintain the modular hierarchy
-flatc -g --gen-all -o "$GO_DIR" "fbs/consolidated.fbs"
+flatc -g --gen-all -o "$GO_DIR/internal" "fbs/consolidated.fbs"
 flatc -p --gen-all -o "$PYTHON_DIR" "fbs/consolidated.fbs"
 
 # Build Rust Core Libraries
@@ -57,7 +57,7 @@ flatc -p --gen-all -o "$PYTHON_DIR" "fbs/consolidated.fbs"
 # Build Rust dynamic link file
 
 # Prepare Build Result Directory
-rm -rf cs/openxml_office/lib && mkdir -p cs/openxml_office/lib
+rm -rf cs/lib && mkdir -p cs/lib
 rm -rf python/lib && mkdir -p python/lib
 rm -rf java/draviavemal_openxml_office/src/lib && mkdir -p java/draviavemal_openxml_office/src/main/resources/lib
 rm -rf go/lib && mkdir -p go/lib
@@ -76,9 +76,9 @@ cargo build $release_flag --target x86_64-unknown-linux-gnu
 # cargo build --release --target x86_64-apple-darwin
 
 # Copy Result binary to CS targets
-cp $win_binary_dir/draviavemal_openxml_office_ffi.dll cs/openxml_office/lib/draviavemal_openxml_office_ffi.dll
-cp $linux_binary_dir/libdraviavemal_openxml_office_ffi.so cs/openxml_office/lib/draviavemal_openxml_office_ffi.so
-cp $target_dir/methods.h cs/openxml_office/lib/methods.h
+cp $win_binary_dir/draviavemal_openxml_office_ffi.dll cs/lib/draviavemal_openxml_office_ffi.dll
+cp $linux_binary_dir/libdraviavemal_openxml_office_ffi.so cs/lib/draviavemal_openxml_office_ffi.so
+cp $target_dir/methods.h cs/lib/methods.h
 
 # Copy Result binary to Python targets
 cp $win_binary_dir/draviavemal_openxml_office_ffi.dll python/lib/draviavemal_openxml_office_ffi.dll
